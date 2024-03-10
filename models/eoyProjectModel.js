0b01b52e8f0df0cc52ce10005d6560e78cf2d6ca
clintjeff2 <clintjeff2@gmail.com>
2024-03-10 17:55:42 +0100

    Modified the eoyProjectModel

--- models/eoyProjectModel.js
+++ models/eoyProjectModel.js
@@ -1,37 +1,37 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const eoyProjectSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A project must have a user(owner) please!"],
-  },
-  level: {
-    type: Number,
-    enum: [200, 300, 400, 601, 602],
-    required: [true, "Please provide your level"],
-  },
-  specialty: {
-    type: String,
-    enum: ["SWE", "ITS", "GWD"],
-    required: [true, "Please provide your specialty"],
-  },
-  projectTopic1: {
-    type: String,
-    required: [true, "Please provide your project topic"],
-  },
-  projectTopic2: {
-    type: String,
-  },
-  projectSupervisor: {
-    type: String,
-    required: [
-      true,
-      "Please propose a supervisor you'll want for your project",
-    ],
-  },
+	name: {
+		type: String,
+		required: [true, 'A project must have a user(owner) please!'],
+	},
+	level: {
+		type: Number,
+		enum: [200, 300, 400, 601, 602],
+		required: [true, 'Please provide your level'],
+	},
+	specialty: {
+		type: String,
+		enum: ['SWE', 'ITS', 'GWD'],
+		required: [true, 'Please provide your specialty'],
+	},
+	projectTopic1: {
+		type: String,
+		required: [true, 'Please provide your project topic'],
+	},
+	projectTopic2: {
+		type: String,
+	},
+	projectSupervisor: {
+		type: String,
+		required: [
+			true,
+			"Please propose a supervisor you'll want for your project",
+		],
+	},
 });
 
 // Creating a model
-const EoyProject = mongoose.model("EoyProject", eoyProjectSchema);
+const EoyProject = mongoose.model('EoyProject', eoyProjectSchema);
 
 module.exports = EoyProject;
