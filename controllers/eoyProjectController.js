dbbe8742eba08aea8d0b8ef8f814fdedf6cd3f8c
clintjeff2 <clintjeff2@gmail.com>
2024-03-10 17:54:22 +0100

    Changed the tours variables to projects

--- controllers/eoyProjectController.js
+++ controllers/eoyProjectController.js
@@ -1,83 +1,84 @@
-const EoyProject = require("../models/eoyProjectModel");
-const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
-const APIFeatures = require("../utils/apiFeatures");
+const EoyProject = require('../models/eoyProjectModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.createEoyProject = catchAsync(async (req, res, next) => {
-  const newProject = await EoyProject.create({
-    name: req.body.name,
-    specialty: req.body.specialty,
-    level: req.body.level,
-    projectTopic: req.body.projectTopic,
-    projectSupervisor: req.body.projectSupervisor,
-  });
+	const newProject = await EoyProject.create({
+		name: req.body.name,
+		specialty: req.body.specialty,
+		level: req.body.level,
+		projectTopic1: req.body.projectTopic1,
+		projectTopic2: req.body.projectTopic2,
+		projectSupervisor: req.body.projectSupervisor,
+	});
 
-  res.status(201).json({
-    status: "Successful",
-    data: {
-      project: newProject,
-    },
-  });
+	res.status(201).json({
+		status: 'Successful',
+		data: {
+			project: newProject,
+		},
+	});
 });
 
 exports.getAllEoyProjects = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(EoyProject.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const tours = await features.query;
+	const features = new APIFeatures(EoyProject.find(), req.query)
+		.filter()
+		.sort()
+		.limitFields()
+		.paginate();
+	const projects = await features.query;
 
-  // SEND RESPONSE
-  res.status(200).json({
-    status: "success",
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+	// SEND RESPONSE
+	res.status(200).json({
+		status: 'success',
+		results: projects.length,
+		data: {
+			projects,
+		},
+	});
 });
 
 exports.getEoyProject = catchAsync(async (req, res, next) => {
-  const project = await EoyProject.findById(req.params.id);
+	const project = await EoyProject.findById(req.params.id);
 
-  if (!project) {
-    return next(new AppError("No project found with this id", 404));
-  }
+	if (!project) {
+		return next(new AppError('No project found with this id', 404));
+	}
 
-  res.status(200).json({
-    stauts: "success",
-    data: {
-      project,
-    },
-  });
+	res.status(200).json({
+		stauts: 'success',
+		data: {
+			project,
+		},
+	});
 });
 
 exports.updateEoyProject = catchAsync(async (req, res, next) => {
-  const project = await EoyProject.findByIdAndUpdate(req.params.id, req.body, {
-    runValidators: true,
-    new: true,
-  });
-  if (!project) {
-    return next(new AppError("No project found with this id", 404));
-  }
+	const project = await EoyProject.findByIdAndUpdate(req.params.id, req.body, {
+		runValidators: true,
+		new: true,
+	});
+	if (!project) {
+		return next(new AppError('No project found with this id', 404));
+	}
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      project,
-    },
-  });
+	res.status(200).json({
+		status: 'success',
+		data: {
+			project,
+		},
+	});
 });
 
 exports.deleteEoyProject = catchAsync(async (req, res, next) => {
-  const project = await EoyProject.findByIdAndDelete(req.params.id);
+	const project = await EoyProject.findByIdAndDelete(req.params.id);
 
-  if (!project) {
-    return next(new AppError("No project found with that ID", 404));
-  }
-  res.status(204).json({
-    status: "Successful",
-    data: null,
-  });
+	if (!project) {
+		return next(new AppError('No project found with that ID', 404));
+	}
+	res.status(204).json({
+		status: 'Successful',
+		data: null,
+	});
 });
