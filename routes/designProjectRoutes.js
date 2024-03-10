bf7fcfb535a7a16c69c8a84d40be8e812d12da17
clintjeff2 <clintjeff2@gmail.com>
2024-03-10 17:56:22 +0100

    Modified the designProjectRoutes.js

--- routes/designProjectRoutes.js
+++ routes/designProjectRoutes.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const projController = require("../controllers/designProjectController");
+const express = require('express');
+const projController = require('../controllers/designProjectController');
 
 const router = express.Router();
 
 router
-  .route("/")
-  .get(projController.getAllDesignProjects)
-  .post(projController.createDesignProject);
+	.route('/')
+	.get(projController.getAllDesignProjects)
+	.post(projController.createDesignProject);
 
 router
-  .route("/:id")
-  .get(projController.getDesignProject)
-  .patch(projController.updateDesignProject)
-  .delete(projController.deleteDesignProject);
+	.route('/:id')
+	.get(projController.getDesignProject)
+	.patch(projController.updateDesignProject)
+	.delete(projController.deleteDesignProject);
 
 module.exports = router;
