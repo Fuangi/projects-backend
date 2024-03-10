2cacc5982a09ee27f5399d41356b3a7f2017f2d9
clintjeff2 <clintjeff2@gmail.com>
2024-03-10 17:53:05 +0100

    Added cors to accept cross-assess origin

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-const express = require("express");
-const path = require("path");
-const eoyProjectRouter = require("./routes/endProjectRoutes");
-const designProjectRouter = require("./routes/designProjectRoutes");
+const express = require('express');
+const path = require('path');
+const eoyProjectRouter = require('./routes/endProjectRoutes');
+const designProjectRouter = require('./routes/designProjectRoutes');
+const cors = require('cors');
 
 const app = express();
 
 // pug
-app.set("view engine", "pug");
+app.set('view engine', 'pug');
 // specifying the location to our html files
-app.set("views", path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, 'views'));
 // Serving static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("css"));
+app.use(express.static('css'));
+
+app.use(cors({ method: 'POST PATCH GET DELETE', origin: 'http://localhost:3000' }));
 
 // body parser - reading data from body into req.body
 app.use(express.json());
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
 }
 
 // Setting up the routes
-app.use("/projects/eoy", eoyProjectRouter);
-app.use("/projects/design", designProjectRouter);
+app.use('/projects/eoy', eoyProjectRouter);
+app.use('/projects/design', designProjectRouter);
 
 // Pug routes
-app.get("/", (req, res) => {
-  res.status(200).render("base");
+app.get('/', (req, res) => {
+	res.status(200).render('base');
 });
 
 // Handline unhandled routes
-app.all("*", (req, res, next) => {
-  res.send("Can't find this route");
+app.all('*', (req, res, next) => {
+	res.send("Can't find this route");
 });
 
 module.exports = app;
